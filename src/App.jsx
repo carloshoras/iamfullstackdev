f52b307c81883ed00fee5b93b33bf390f2d51920
carloshoras <carloshoras>
2024-04-18 12:32:10 +0200

    Ejercicio Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './Home.jsx'
 import ItemDetailPage from "./ItemDetailPage.jsx";
-
+import InputCreate from "./InputCreate.jsx";
 
 const App = () => {
   const [data, setData] = useState(null)
@@ -27,6 +27,7 @@ useEffect(() => {
       <div>
         <nav>
           <Link to="/">Inicio</Link>
+          <Link to="/create">Crear Tarea</Link>
      
         </nav>
         {data === null 
@@ -34,7 +35,7 @@ useEffect(() => {
         : 
           <Routes>
             <Route path="/" element={<Home data={data} />} />
-           
+            <Route path="/create" element={<InputCreate/>} />
             {data.map(item => (
               <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
             ))
---
+++ src/InputCreate.jsx
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react'
+function InputCreate () {
+    const [inputValue, setInputValue] = useState("")
+    const [clickedButton, setClickedButton] = useState(true)
+    const urlApi = 'http://localhost:3000/create'
+    useEffect(() => {
+        const postNewTask = async () => {
+            const payload = {title: inputValue}
+            fetch(urlApi, {
+                method: 'POST', // Método HTTP
+                headers: {
+                    'Content-Type': 'application/json', // Indicamos que el contenido es JSON
+                },
+                body: JSON.stringify(payload), // Convertimos el payload de JS a JSON
+            })
+            setInputValue("")
+        }
+        if (inputValue.trim() !== "") {
+            postNewTask()
+        }
+    },[clickedButton])
+    return (
+        <>
+        <input 
+        type="text" 
+        placeholder="Introduce nueva tarea"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}/>
+        <button type="button" onClick={() => setClickedButton(!clickedButton)}>Agregar Tarea</button>
+        </>
+    )
+}
+
+export default InputCreate
